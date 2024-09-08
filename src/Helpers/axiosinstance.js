2c3a07947707469a2ffb61db16a199af94f2ed2a
Tushar Sharma <tusharsharma5211@gmail.com>
2024-09-08 16:01:44 +0530

    Added footer component

---
+++ src/Helpers/axiosinstance.js
@@ -0,0 +1,2 @@
+import axios from 'axios';
+BASE_URL = 'http://localhost:5173/api/v1';
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+//Component imports
+import App from './App.jsx';
+//CSS imports
+import './index.css';
+//Library imports
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
-)
+  </BrowserRouter>
+);
